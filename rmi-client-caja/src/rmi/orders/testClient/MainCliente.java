ad5e591221a54a7c8d57222e5dee035e2147f683
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2021-06-25 03:21:04 -0500

    Conexión correcta cliente-servidor con errores de inserción a la bd

--- rmi-client-caja/src/rmi/orders/testClient/MainCliente.java
+++ rmi-client-caja/src/rmi/orders/testClient/MainCliente.java
@@ -1,5 +1,9 @@
 package rmi.orders.testClient;
 
+import com.ordersmanagement.comun.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -10,18 +14,56 @@ import rmi.orders.api.*;
 import com.ordersmanagement.comun.*;
 
 
-public class MainCliente {
+public class MainCliente{
 	
 	/*Deben utilizar la excepcion RemoteException cada vez que utilicen el objeto servidor*/
 	
 	private static IServidorCaja servidor;
+	private static PedidoCrearDTO pedido;
+	private static LineaPedidoDTO lineaPedido;
+	private static GUICliente gui;
+	private static int exitoPedido;
 	
 	public static void main(String[] args) throws Exception {
+		
+		gui = new GUICliente();
+		ActionListener action = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == gui.getBtnAddOrder()) {
+					pedido = gui.getPedidoGUI();
+					lineaPedido = gui.getLineaPedidoGUI();
+					pedido.anadirLineaPedido(lineaPedido);
+				}
+				
+				if(e.getSource() == gui.getBtnSendOrder()) {
+					try {
+						if(pedido != null) {
+							crearPedido();
+							System.out.print(exitoPedido);	
+						}
+					}
+					catch(RemoteException | SQLException ex) {
+						ex.printStackTrace();
+					}
+				}
+				
+			}
+			
+		};
+		
+		gui.addAL(action);
 		
 		Registry registry = LocateRegistry.getRegistry(3333);
 		servidor = (IServidorCaja)registry.lookup("TestServer");
 		
-		
+	}
+	
+
+	
+	public static void crearPedido() throws RemoteException, SQLException{
+		exitoPedido = servidor.crearPedido(pedido);
 	}
 	
 	
